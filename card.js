eb6489f9028bedc98cc12264bf9fbe6c1cbe044d
Alen1ka <57345786+Alen1ka@users.noreply.github.com>
2020-07-14 22:54:02 +0700

    Add files via upload

---
+++ card.js
@@ -0,0 +1,11 @@
+var wrapper= new Vue({
+    el: '#wrapper',
+    data: {
+        view: true,
+        cardcode: 'XXXX XXXX XXXX XXXX',
+        name: 'FULL NAME',
+        month: 'MM',
+        year: 'YY',
+        cvv: '',
+    }
+})
